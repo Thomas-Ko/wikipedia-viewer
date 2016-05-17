3f74ced9ff21f7d2b9336e72bab904d8adf6494c
Thomas-Ko <tomkolodzi@gmail.com>
2016-05-17 15:56:44 -0400

    Add setSearchResults function and model

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
 /*=========================
     MODEL
 =========================*/
+var model = {
+    
+   /*  results will be filled in by the controller.setSearchResults function when it is invoked by a successful API call.
+    It will contain 10 objects, each with properties title, info, and link.*/
+    results : [],
 
+
+    //this object constructor will be used in the controller.setSearchResults function
+    ResultObject : function (title, info, link) {
+        this.title = title;
+        this.info = info;
+        this.link = link;
+    }
+
+
+
+
+
+        
+
+    
+};
 
 /*=========================
     CONTROLLER
@@ -31,9 +52,26 @@ controller = {
 
             success: function( response ) {
                 console.log(response);
+                controller.setSearchResults(response);
             },
         });
-    } 
+    },
+
+    setSearchResults: function(data){
+        for (i=0; i<10; i++){
+            var arr=[];
+            for (x=1; x<data.length; x++){
+                arr.push(data[x][i]);
+            }
+
+            // model.results.push(newArr);
+            var newObj = new model.ResultObject(arr[0],arr[1],arr[2]);
+            model.results.push(newObj);
+
+        }
+    },
+
+
 };
 
        
@@ -48,7 +86,10 @@ view = {
             console.log(inputText);
             controller.getWikiInfo(inputText);
         });
-    }
+    },
+
+
+
 };
 
 
@@ -56,6 +97,5 @@ view = {
     INITIALIZE
 =========================*/
 controller.init();
-
 
 
